de356518fdcd150d1587127883b2e2037bf8c480
Dunkelmann <bernd.schmecka@gmail.com>
2021-07-16 18:26:22 +0200

    Made MatchSVFunction returning bool and out Match

--- MarkdownToLatex/MarkdownToLatex/MathParser.cs
+++ MarkdownToLatex/MarkdownToLatex/MathParser.cs
@@ -11,15 +11,17 @@ namespace MarkdownToLatex {
         public static Dictionary<string, Regex> MathRx {get;}
 
         /// <summary>Checks if an <paramref name="element"/> is a single variable function.</summary>
-        /// <returns>a Match indicating whether the <paramref name="element"/> is a single variable function or not.</returns>
-        public static Match MatchSVFunction(string element) {
+        /// <returns>a bool indicating whether the <paramref name="element"/> is a single variable function or not and the fitting <paramref name="match"/>.</returns>
+        public static bool MatchSVFunction(string element, out Match match) {
             Regex svregex;
             bool getregex = MathRx.TryGetValue("svfunction", out svregex);
             if(getregex){
-                return svregex.Match(element);
+                match = svregex.Match(element);
+                return true;
             } else {
                 Console.WriteLine("[ERROR] Error getting RegEx.");
-                return Match.Empty;
+                match = Match.Empty;
+                return false;
             }
         }
 

65d8b7c53335682c4ee49d0bf04300181c7ae706
Dunkelmann <bernd.schmecka@gmail.com>
2021-07-17 21:58:43 +0200

    Switch to english tests for consistency reasons

--- MarkdownToLatex/MarkdownToLatex.Test/TestLatexRenderer.cs
+++ MarkdownToLatex/MarkdownToLatex.Test/TestLatexRenderer.cs
@@ -7,10 +7,10 @@ namespace MarkdownToLatex.Test
     {
 
         [Theory]
-        [InlineData("# Der Tragödie erster Teil", @"\chapter*{Der Tragödie erster Teil}")]
-        [InlineData("## Vor dem Tor - Osterspaziergang", @"\section*{Vor dem Tor - Osterspaziergang}")]
-        [InlineData("### Alfred Ill", @"\subsection*{Alfred Ill}")]
-        [InlineData("#### k0m!5(hЗr !npu7", @"\subsection*{k0m!5(hЗr !npu7}")]
+        [InlineData("# A nice Chapter", @"\chapter*{A nice Chapter}")]
+        [InlineData("## A nicer Section", @"\section*{A nicer Section}")]
+        [InlineData("### An even nicer Subsection", @"\subsection*{An even nicer Subsection}")]
+        [InlineData("#### vërÿ wëïrd ïnpüt", @"\subsection*{vërÿ wëïrd ïnpüt}")]
         public void TestWriteHeadline(string mdline, string texline){
             LatexRenderer.WriteHeadline(MarkdownParser.MatchHeadline(mdline));
             Assert.Contains(texline, LatexRenderer.LatexLines);
@@ -20,20 +20,20 @@ namespace MarkdownToLatex.Test
         public void TestWriteList(){
             LatexRenderer.LatexLines.Clear();
             string[] mdlines = {
-                "- Hallo, dies ist ein Test",
-                "- mit einer Liste",
-                "- die wirklich sehr, sehr toll ist",
-                "- nicht wahr?",
-                "- Ok, das war's auch schon!"
+                "- Hello, this is a test",
+                "- with a list",
+                "- which really is very nice",
+                "- or is it?",
+                "- ok, that was it for now!"
             };
 
             string[] texlines = {
                 @"\begin{itemize}",
-                @"\item{Hallo, dies ist ein Test}",
-                @"\item{mit einer Liste}",
-                @"\item{die wirklich sehr, sehr toll ist}",
-                @"\item{nicht wahr?}",
-                @"\item{Ok, das war's auch schon!}",
+                @"\item{Hello, this is a test}",
+                @"\item{with a list}",
+                @"\item{which really is very nice}",
+                @"\item{or is it?}",
+                @"\item{ok, that was it for now!}",
                 @"\end{itemize}"
             };
 
@@ -48,14 +48,14 @@ namespace MarkdownToLatex.Test
         public void TestWriteQuote(){
             LatexRenderer.LatexLines.Clear();
             string[] mdlines = {
-                "> Probleme kann man niemals mit derselben Denkweise lösen, durch die sie entstanden sind.",
-                "> - Albert Einstein"
+                "> Life is what happens when you're busy making other plans.",
+                "> - John Lennon"
             };
 
             string[] texlines = {
                 @"\begin{quote}",
-                "Probleme kann man niemals mit derselben Denkweise lösen, durch die sie entstanden sind.",
-                "- Albert Einstein",
+                "Life is what happens when you're busy making other plans.",
+                "- John Lennon",
                 @"\end{quote}"
             };
 
@@ -67,9 +67,9 @@ namespace MarkdownToLatex.Test
         }
 
         [Theory]
-        [InlineData("Das hast *du* gesagt, __nicht__ ich!", @"Das hast \textit{du} gesagt, \textbf{nicht} ich!")]
-        [InlineData("***WARNUNG:*** Bitte **immer** Handschuhe tragen (_keine_ Stoffhandschuhe!)", @"\textbf{\textit{WARNUNG:}} Bitte \textbf{immer} Handschuhe tragen (\textit{keine} Stoffhandschuhe!)")]
-        [InlineData("Alle guten Dinge sind **drei**, *nicht* zwei!", @"Alle guten Dinge sind \textbf{drei}, \textit{nicht} zwei!")]
+        [InlineData("Hello *World*!", @"Hello \textit{World}!")]
+        [InlineData("***WARNING:*** Please do **always** wear gloves (_important_!)", @"\textbf{\textit{WARNING:}} Please do \textbf{always} wear gloves (\textit{important}!)")]
+        [InlineData("All good things are **three**, *not* two!", @"All good things are \textbf{three}, \textit{not} two!")]
         public void TestBoldCursive(string mdinput, string texoutput){
             string result = LatexRenderer.WriteCursive(LatexRenderer.WriteBold(mdinput));
             Assert.Equal(texoutput, result);

0307020bdae4bf0561c53e75f4538a77b692f664
fb89zila <73189649+fb89zila@users.noreply.github.com>
2021-07-18 20:32:24 +0200

    changes directory separator char

--- MarkdownToLatex/MarkdownToLatex.Test/TestMdToTex.cs
+++ MarkdownToLatex/MarkdownToLatex.Test/TestMdToTex.cs
@@ -7,13 +7,15 @@ namespace MarkdownToLatex.Test
     
     public class TestMdToTex
     {
+        private readonly char dirSep = Path.DirectorySeparatorChar;
+        
         [Fact]
         public void TestParsePath(/*string path, string expPath*/)
         {
             //arrange
             string fileName = "path_test.md";
             string path = @"test_files"; 
-            string expPath = @"test_files\path_test.md";
+            string expPath = @"test_files/path_test.md";
 
             //act
             string filePath = Path.Combine(path, fileName);

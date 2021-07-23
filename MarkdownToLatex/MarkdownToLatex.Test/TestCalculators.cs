f83105d84c1bf7c7633cfcb7884429a4f76952a6
fb89zila <fb89zila@student.tu-freiberg.de>
2021-07-23 19:29:03 +0200

    add CalcDerivative() test

--- MarkdownToLatex/MarkdownToLatex.Test/TestCalculators.cs
+++ MarkdownToLatex/MarkdownToLatex.Test/TestCalculators.cs
@@ -49,11 +49,44 @@ namespace MarkdownToLatex.Test
             Assert.Equal(expected, result.ToArray());
         }
 
+        [Fact]
+        public void TestCalcDerivative()
+        {
+            //arrange
+            FuncCalculator calc = new FuncCalculator("x", "2*x^4-4*x^2+5*x-3");
+            MatchCollection mc = MathParser.MatchParameters("{d1f}{d1f(0)}{d1f(2e-3)[3]}{d2f}{d2f(0)}{d2f(2e-3)[3]}");
+            List<string> result = new List<string>();
+            string[] expected = new string[] {
+                @"f'(x)=5 - 8x + 8{x}^{3}",
+                @"f'(0)=5",
+                @"f'(0.002)=4.984",
+                @"f''(x)=-8 + 24{x}^{2}",
+                @"f''(0)=-8",
+                @"f''(0.002)=-8"
+            };
+
+            //act
+            foreach (Match m in mc) {
+                if (m.Groups[1].Value == "d1f") {
+                    result.Add(calc.CalcDerivative(m, 1));
+                } else {
+                    result.Add(calc.CalcDerivative(m, 2));
+                }
+            }
+
+            //assert
+            Assert.Equal(expected, result.ToArray());
+        }
+
         [Theory]
         [InlineData("x", "x^3-2*x^2+sin(x)", @"f(x)=-2{x}^{2} + {x}^{3} + \sin{x}")]
         [InlineData("t", "(sin(t))^2+(cos(t))^2", @"f(t)=\sin^{2}{t} + \cos^{2}{t}")]
-        public void TestConvertFunction(string var, string function, string output){
+        public void TestConvertFunction(string var, string function, string output)
+        {
+            //arrange
             FuncCalculator calc = new FuncCalculator(var, function);
+
+            //assert
             Assert.Equal(output, calc.ConvertElement());
         }
 
